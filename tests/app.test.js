07f1b8a284d427736092e4de67dc2e8a6ce5dcfb
sachingit607 <168462916+sachingit607@users.noreply.github.com>
2024-12-25 23:19:35 +0530

    Fixed Kubernets issue

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,4 @@
 // const request = require('supertest');
-// const app = require('../index'); // This will be the Express app
-
-// describe('GET /', () => {
-//   it('should return Hello World!', async () => {
-//     const response = await request(app).get('/');
-//     expect(response.text).toBe('Hello World!');
-//   });
-// });
 const request = require('supertest');
 const app = require('../index'); // Import app from index.js (not starting the server)
 
